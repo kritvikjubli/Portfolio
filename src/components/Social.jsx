13756b08f0d2ba85928c662044a8e1594208e146
kritvik Jubli <kritvikjubli@gmail.com>
2024-09-24 11:47:10 +0530

    commit resume added

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { FaGithub,FaLinkedin } from 'react-icons/fa'
+import { FaFax, FaGithub,FaLinkedin, } from 'react-icons/fa'
+import resume from '../assets/resume.pdf'
 import{HiOutlineMail} from "react-icons/hi"
+
 const Social = () => {
   return (
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
+         <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-xl duration-[400ms] '>
+            <a className='flex justify-between items-center w-full text-white' href={resume} download="resume" target="_blank" rel="noreferrer" > 
+         <>Resume  <FaFax size={30}/> </>   
+         </a>
+         </li>
         <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-xl duration-[400ms] '>
             <a className='flex justify-between items-center w-full text-white' href="https://www.linkedin.com/in/kritvik-jubli-668118228/" target="_blank" rel="noreferrer" > 
          <>Likedin  <FaLinkedin size={30}/> </>   
